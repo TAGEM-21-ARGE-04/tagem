0a28e3aff7510cad236c5bb7f072c360e3464105
Omer Demirtas <gamega55834@gmail.cm>
2023-05-23 18:50:08 +0300

    Init Application

---
+++ src/main/java/com/sau/tagem/TagemApplication.java
@@ -0,0 +1,13 @@
+package com.sau.tagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sau/tagem/TagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sau.tagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
