5984c611a0bbbcf25f0adf4c73841642c64ed9e8
Omer Demirtas <gamega55834@gmail.cm>
2023-05-23 20:06:40 +0300

    Group Repository added

---
+++ src/main/java/com/sau/tagem/model/Group.java
@@ -0,0 +1,29 @@
+package com.sau.tagem.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "t_group")
+public class Group {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(mappedBy = "group")
+    List<Flower> flowers;
+}
---
+++ src/main/java/com/sau/tagem/repository/GroupRepository.java
@@ -0,0 +1,6 @@
+package com.sau.tagem.repository;
+
+import com.sau.tagem.model.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> { }
