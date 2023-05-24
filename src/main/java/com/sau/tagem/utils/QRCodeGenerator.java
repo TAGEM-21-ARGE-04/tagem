e7846db5ebf7caa9a04ff58e3698d00d0256835b
Ömer Demirtaş <47040453+Omer-Demirtas@users.noreply.github.com>
2023-05-24 23:30:51 +0300

    QR code generator updated

--- src/main/java/com/sau/tagem/utils/QRCodeGenerator.java
+++ src/main/java/com/sau/tagem/utils/QRCodeGenerator.java
@@ -5,6 +5,7 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -13,13 +14,11 @@ import java.nio.file.Path;
 
 public class QRCodeGenerator {
 
-    public static void generateQRCodeImage(String text, int width, int height, String filePath) throws WriterException, IOException {
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+    @Value("${app.default.qr.width}")
+    static Integer WIDTH;
 
-        Path path = FileSystems.getDefault().getPath(filePath);
-        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
-    }
+    @Value("${app.default.qr.height}")
+    static Integer HEIGHT;
 
 
     public static byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
@@ -31,5 +30,12 @@ public class QRCodeGenerator {
         return pngOutputStream.toByteArray();
     }
 
+    public static byte[] getQRCodeImage(String text) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 100, 100);
 
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        return pngOutputStream.toByteArray();
+    }
 }
