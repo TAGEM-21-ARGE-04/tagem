67aa4a435c9a5dbc8d65630848a7c040e1a85355
Ömer Demirtaş <47040453+Omer-Demirtas@users.noreply.github.com>
2023-05-24 22:05:04 +0300

    GroupController updated

--- src/main/java/com/sau/tagem/controller/GroupController.java
+++ src/main/java/com/sau/tagem/controller/GroupController.java
@@ -3,7 +3,10 @@ package com.sau.tagem.controller;
 import com.sau.tagem.dto.GroupDTO;
 import com.sau.tagem.model.Group;
 import com.sau.tagem.service.GroupService;
+import com.sau.tagem.utils.GenericResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class GroupController {
     private final GroupService groupService;
 
-    public GroupDTO save(GroupDTO groupDTO) {
-        return groupService.save(groupDTO);
+    @PostMapping
+    public GenericResponse<GroupDTO> save(@RequestBody GroupDTO groupDTO) {
+        return GenericResponse.success(groupService.save(groupDTO));
     }
 }
