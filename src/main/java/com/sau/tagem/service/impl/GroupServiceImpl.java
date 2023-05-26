06b88d5f3df7248b95ff1ca7052b7d683291407b
Ömer Demirtaş <47040453+Omer-Demirtas@users.noreply.github.com>
2023-05-26 18:10:10 +0300

    Group save process updated

--- src/main/java/com/sau/tagem/service/impl/GroupServiceImpl.java
+++ src/main/java/com/sau/tagem/service/impl/GroupServiceImpl.java
@@ -1,14 +1,18 @@
 package com.sau.tagem.service.impl;
 
 import com.sau.tagem.dto.GroupDTO;
+import com.sau.tagem.model.Flower;
 import com.sau.tagem.model.Group;
 import com.sau.tagem.repository.GroupRepository;
+import com.sau.tagem.service.FlowerService;
 import com.sau.tagem.service.GroupService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,13 +21,22 @@ import java.util.Optional;
 public class GroupServiceImpl implements GroupService {
 
     private final ModelMapper modelMapper;
+    private final FlowerService flowerService;
     private final GroupRepository groupRepository;
 
     @Override
+    @Transactional
     public GroupDTO save(GroupDTO groupDTO) {
         Group group = groupRepository.save(
                 modelMapper.map(groupDTO, Group.class)
         );
+
+        group.setFlowers(new ArrayList<>());
+
+        for (int i = 1; i < groupDTO.getFlowerCount() + 1; i++) {
+            Flower flower = flowerService.save(new Flower(i, group));
+            group.getFlowers().add(flower);
+        }
 
         return modelMapper.map(group, GroupDTO.class);
     }
